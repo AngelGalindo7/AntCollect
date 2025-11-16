d267f372970152d0198bd4470072ab201e2f7884
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-11-16 00:01:52 -0800

    feat(utils/auth and utils/files): Added helper functions for auth(hash_password,create_access_token) and files(save uploaded images to disk and get_file_size)

---
+++ utils/files.py
@@ -0,0 +1,30 @@
+import os 
+import shutil 
+from fastapi import UploadFile, HTTPException
+
+UPLOAD_DIR = "Uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+def save_upload_file(file: UploadFile) -> str:
+
+    if not file.filename:
+        raise HTTPException(status_code=400, detail="File must have a filname")
+    if not file.content_type or not file.content_type.startswith("image/"):
+        raise HTTPException(status_code=400, detail="File must be an image")
+
+    #TODO: Add exception for large file
+
+    file_path = os.path.join(UPLOAD_DIR, file.filename)
+
+    try:
+        with open(file_path, "wb") as buffer:
+            shutil.copyfileobj(file.file, buffer)
+        
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to save file: {e}")
+    
+    return file_path
+
+def get_file_size(file_path:str) -> int:
+    """Return file size in bytes"""
+    return os.path.getsize(file_path)
