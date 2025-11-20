cf809e2fd0ea091f7044c8548c9d95c975aaead0
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-11-19 23:57:10 -0800

    feat(models): Minor change

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import uuid
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON, Text,func
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON, Text,func, Boolean
 from sqlalchemy.dialects.postgresql import UUID, JSONB
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
@@ -10,9 +10,9 @@ class User(Base):
     __tablename__ = "users"
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] =  mapped_column(String(50), nullable=False)
-    email: Mapped[str] = mapped_column(String(50), nullable=False)
+    email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(Text, nullable=False)
-    created_at: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
 class Image(Base):
     __tablename__ = "images"
@@ -23,4 +23,13 @@ class Image(Base):
     mime_type: Mapped[str] = mapped_column(String(50), nullable=False)
     size_bytes: Mapped[int] = mapped_column(Integer, nullable=False)
     json_metadata: Mapped[dict | None] = mapped_column(JSONB, nullable=True)
-    upload_time: Mapped[datetime.datetime] = mapped_column(DateTime, server_default=func.now())
+    upload_time: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+class RefreshToken(Base):
+    __tablename__ = "refresh_tokens"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
+    token: Mapped[str] = mapped_column(String, unique=True, nullable=False)
+    issued_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    expires_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=False)
+    revoked: Mapped[bool] = mapped_column(Boolean, default=False)
