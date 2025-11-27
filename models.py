f02733c2d96025cca7eefdfca188ab50c9c183fc
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-11-26 23:43:47 -0800

    feat(models.py): Added posts,post_comments,post_likes and refactored upload table to post_images

--- models.py
+++ models.py
@@ -1,23 +1,37 @@
 import uuid
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, JSON, Text,func, Boolean
 from sqlalchemy.dialects.postgresql import UUID, JSONB
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from database import Base
 import datetime
 
 
 class User(Base):
     __tablename__ = "users"
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     username: Mapped[str] =  mapped_column(String(50), nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(Text, nullable=False)
     created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
-class Image(Base):
-    __tablename__ = "images"
-    id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    user_id: Mapped[int | None]  = mapped_column(Integer, ForeignKey("users.id"), nullable=True)
+
+#For caption use of Text, vs String etc
+class Post(Base):
+    __tablename__ = "posts"
+    post_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    caption: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    is_published: Mapped[bool] = mapped_column(Boolean, server_default="True")
+
+    images: Mapped[list["PostImage"]] = relationship("PostImage", back_populates="post",cascade="all, delete-orphan")
+
+#Change to PostImages
+class PostImage(Base):
+    __tablename__ = "post_images"
+    post_image_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("posts.post_id"), nullable = False)
     filename: Mapped[str] = mapped_column(String(255), nullable=False)
     file_path: Mapped[str] = mapped_column(String(255), nullable=False)
     mime_type: Mapped[str] = mapped_column(String(50), nullable=False)
@@ -25,11 +39,35 @@ class Image(Base):
     json_metadata: Mapped[dict | None] = mapped_column(JSONB, nullable=True)
     upload_time: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
 
+    post: Mapped["Post"] = relationship("Post", back_populates="images")
+
+
+
 class RefreshToken(Base):
     __tablename__ = "refresh_tokens"
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
     user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable=False)
     token: Mapped[str] = mapped_column(String, unique=True, nullable=False)
     issued_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
     expires_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), nullable=False)
     revoked: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    
+
+class PostLike(Base):
+    __tablename__ = "post_likes"
+    like_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("posts.post_id"))
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"), nullable = False)
+    liked_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+class PostComment(Base):
+    __tablename__ = "post_comments"
+    comment_id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    post_id: Mapped[int] = mapped_column(Integer, ForeignKey("posts.post_id"))
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    content: Mapped[str] = mapped_column(Text)
+    created_at: Mapped[datetime.datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+
+
+#TODO Add a BAN TABLE
