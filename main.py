7fef7816b38a837eb52ad183ccb9747fffbe35e3
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-12-05 21:31:38 -0800

    refactor(main.py): cleanup imports

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-from fastapi import FastAPI, UploadFile, File, Depends, Form, HTTPException
+from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from sqlalchemy import text
-from sqlalchemy.exc import IntegrityError
 from database import get_db
-from models import PostImage, User, RefreshToken, Post, PostLike, PostComment
-import shutil, os
-import os
-from utils.files import save_upload_file, get_file_size, delete_file
-from schemas import UserCreate, UserResponse, UserLogin, TokenResponse, RefreshRequest, AuthorizeTokenResponse
-from utils.auth import hash_password, verify_password, create_access_token, create_refresh_token,valid_refresh_token,authenthicate_access_token
 from routers import auth, users, posts
 
 app = FastAPI()
