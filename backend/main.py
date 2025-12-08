b0dad33045d9bffc1a440589eba265ad9993a723
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2025-12-07 19:33:36 -0800

    feat(backend/main): Added cors middle to allow frontend origin

--- backend/main.py
+++ backend/main.py
@@ -3,12 +3,23 @@ from sqlalchemy.orm import Session
 from sqlalchemy import text
 from .database import get_db
 from .routers import auth, users, posts
-
+from fastapi.middleware.cors import CORSMiddleware
 #. relative import current package .. import from parent package
 #packages make relative imports reliable
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
