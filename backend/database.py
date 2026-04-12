9f1ea3a7657ce145a230b78679c797840476b3f5
Angel Galindo <131726962+AngelGalindo7@users.noreply.github.com>
2026-04-11 17:45:24 -0700

    chore(backend database): simplify engine creation

--- backend/database.py
+++ backend/database.py
@@ -12,10 +12,8 @@ DB_PASSWORD = quote_plus(os.getenv("DB_PASSWORD", ""))
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DB_SSLMODE = os.getenv("DB_SSLMODE", "prefer")
-
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-engine = create_engine(DATABASE_URL, connect_args={"sslmode": DB_SSLMODE})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
